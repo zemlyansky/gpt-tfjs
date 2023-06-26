ea915abce51debe1afc6479665752e890abb8e7d
zemlyansky <anton.zemlyansky@gmail.com>
2023-06-26 12:30:30 +0200

    Add maxIter parameter and switch from forEachAsync to manual iteration over a TF dataset
    Previously used forEachAsync made it hard to early stop when maxIter is reached

--- src/train.js
+++ src/train.js
@@ -3,7 +3,8 @@ const { AdamW, clipByGlobalNorm, clipByGlobalNormObj, l2Loss } = require('./opti
 
 async function train (model, ds, config) {
     const defaultConfig = {
-        epochs: 5,
+        epochs: null,
+        maxIter: null,
         batchSize: 16,
         shuffle: true,
         lr: 6e-4,
@@ -43,31 +44,62 @@ async function train (model, ds, config) {
     } else {
         var opt = tf.train.adam(6e-4)
     }
-    
-    for (let epoch = 1; epoch <= config.epochs; epoch++) {
-        if (config.verbose) { console.log('\nEpoch', epoch) }
-        
-        var losses = []
-        await ds.forEachAsync(({x, y}) => {
-            var optFunc = () => {
-                const logits = model.apply(x)
-                const loss = tf.losses.softmaxCrossEntropy(y, logits)
-                const lossValue = loss.arraySync()
-                losses.push(lossValue)
-                return loss
+
+    let epoch = 1
+    let iteration = 1
+    let iterator = await ds.iterator()
+
+    while (true) {
+        let next = await iterator.next()
+        if (next.done) {
+            epoch++
+            if (config.epochs && epoch > config.epochs) {
+                break
             }
-            tf.tidy(() => {
-                var {values, grads} = opt.computeGradients(optFunc)
-                var gradsClipped = clipByGlobalNormObj(grads, 1)
-                opt.applyGradients(gradsClipped)
-            })
+            iterator = await ds.iterator()
+            next = await iterator.next()
+        }
+        const {x, y} = next.value
+
+        // Keep loss for reporting
+        let loss
+        const optFunc = () => {
+            const logits = model.apply(x)
+            loss = tf.keep(tf.losses.softmaxCrossEntropy(y, logits))
+            return loss
+        }
+        tf.tidy(() => {
+            let {values, grads} = opt.computeGradients(optFunc)
+            let gradsClipped = clipByGlobalNormObj(grads, 1)
+            opt.applyGradients(gradsClipped)
         })
 
-        if (config.verbose) {
-            console.log('Mem:', tf.memory().numTensors)
-            console.log('Loss:', tf.tidy(() => tf.mean(losses).arraySync()))
+        let lossVal = await loss.array()
+        if (Array.isArray(config.callbacks)) {
+            for (let callback of config.callbacks) {
+                await callback(model, lossVal, iteration)
+            }
         }
+
+        // Dispose everything
+        loss.dispose()
+        x.dispose()
+        y.dispose()
+
+        // Check if we should stop
+        iteration++
+        if (config.maxIter && iteration > config.maxIter) {
+            break
+        }
+
+        if (config.verbose) {
+            console.log('Mem:', tf.memory())
+            console.log(`Epoch: ${epoch}, Step: ${iteration}, Loss: ${lossVal}`)
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 1))
     }
+
 }
 
 module.exports = {
